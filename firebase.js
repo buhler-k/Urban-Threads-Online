6a77e5a0431957e4bc4fc1742086eb70eeee382b
BUHLE <buhlentombikhoza@gmail.com>
2026-08-12 18:46:47 +0200

    Made modifications to the cart, login, firebase and style files

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCNT3yecstHydsZ1e0vRyeZHbO8CerSnk",
+  authDomain: "urbanthreadsonlinestore-62420.firebaseapp.com",
+  projectId: "urbanthreadsonlinestore-62420",
+  storageBucket: "urbanthreadsonlinestore-62420.firebasestorage.app",
+  messagingSenderId: "835446695896",
+  appId: "1:835446695896:web:ee45c0b61c5d688699b4ad"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCCNT3yecstHydsZ1e0vRyeZHbO8CerSnk",
-    authDomain: "urbanthreadsonlinestore-62420.firebaseapp.com",
-    projectId: "urbanthreadsonlinestore-62420",
-    storageBucket: "urbanthreadsonlinestore-62420.firebasestorage.app",
-    messagingSenderId: "835446695896",
-    appId: "1:835446695896:web:ee45c0b61c5d688699b4ad"
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+export default app;
