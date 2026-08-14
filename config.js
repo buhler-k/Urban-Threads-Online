98c636b0140929f881a1a98cf58aea0150de884f
BUHLE <buhlentombikhoza@gmail.com>
2026-08-14 04:09:57 +0200

    removed config on git ignore

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCnUNUyMtX63UGFGC0_blac5a_SkYkxpHQ",
+  authDomain: "urbanthreadsonlinestore-62420.firebaseapp.com",
+  projectId: "urbanthreadsonlinestore-62420",
+  storageBucket: "urbanthreadsonlinestore-62420.firebasestorage.app",
+  messagingSenderId: "835446695896",
+  appId: "1:835446695896:web:ee45c0b61c5d688699b4ad"
+};
